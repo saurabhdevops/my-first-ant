fc9ff0e10011430d212ea77d4bf358900c5ae7e0
Saurabh Oza <saurabh.devops@gmail.com>
2016-09-04 18:59:28 +0000

    My first ant project

---
+++ src/main/java/com/aots/learn/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.aots.learn;
+
+public class HelloWorld{
+	public HelloWorld(){
+		System.out.println("Hello World..!!");
+	}
+	public static void main(String args[]){
+		new HelloWorld();
+	}
+}
